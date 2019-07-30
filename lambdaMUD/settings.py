cf6d489bbfb9737e25980ad36e0833b42d707cd2
larynab <lunchbox1990@hotmail.com>
2019-07-30 10:03:59 -0700

    packages, api, and urls

--- lambdaMUD/settings.py
+++ lambdaMUD/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'otwavr*2!s14x5lxp2^qa&stq9(wcgfc&(+1qe-je9uzykmhky'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'adv_room',
 ]
 
 MIDDLEWARE = [
