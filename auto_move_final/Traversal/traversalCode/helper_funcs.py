353864626c2cbd06e844386d95b00c6713af620e
Laryna Billinghurst <Lunchbox1990@hotmail.com>
2019-08-02 09:11:05 -0700

    final

--- auto_move_final/Traversal/traversalCode/helper_funcs.py
+++ auto_move_final/Traversal/traversalCode/helper_funcs.py
@@ -27,8 +27,42 @@ def getPathToRoom(map, starting_room, target_room):
 
 # return travel data
 def travel(map, current_room, dir):
-	r = requests.post(f"{base_url}/move/", headers = headers, json= {"direction": dir, "next_room_id": f"{map[current_room][dir]}"}) 
+	print(current_room, map[f'{current_room}'], dir)
+	r = requests.post(f"{base_url}/move/", headers = headers, json= {"direction": dir, "next_room_id": f"{map[f'{current_room}'][dir]}"}) 
 	print(r)
 	data = r.json()
 	time.sleep(data["cooldown"])
 	return  data
+
+def getStatus():
+	r = requests.post(f"{base_url}/status/", headers = headers) 
+	status = r.json()
+	print(status)
+	time.sleep(status["cooldown"])
+	return status
+
+def toStore(map, current_room):
+	store_path = getPathToRoom(map, current_room, "1")
+	current_room_data = {}
+	room = current_room
+	for dir in store_path:
+		current_room_data = travel(map, room, dir)
+		room = current_room_data["room_id"]
+	return current_room_data
+
+def sellItems(items):
+	for item in items:
+		r = requests.post(f"{base_url}/sell/", headers = headers, json = {"name": item, "confirm": "yes"}) 
+		sell_data = r.json()
+		time.sleep(sell_data["cooldown"])
+		
+def getItems(items):
+	status = {}
+	for item in items:
+		r = requests.post(f"{base_url}/take/", headers = headers, json = {"name": item}) 
+		sell_data = r.json()
+		time.sleep(sell_data["cooldown"])
+		status = getStatus()
+		if status["encumbrance"] >= status["strength"]//2:
+			break
+	return status
